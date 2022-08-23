6c867e86e6ee855ddc85b08627b041b5d8a67fa8
Alexey Razmanov <razmanov666@gmail.com>
2022-08-23 18:14:12 +0300

    add permissions for classes

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,25 +1,47 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
+# from rest_framework import viewsets
+# from rest_framework.decorators import action
+# from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from .models import Category
 from .models import Women
+from .permissions import IsAdminOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
+# from .models import Category
 
-class WomenViewSet(viewsets.ModelViewSet):
-    # queryset = Women.objects.all()
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
-    def get_queryset(self):
-        pk = self.kwargs.get("pk")
 
-        if not pk:
-            return Women.objects.all()
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
-        return Women.objects.filter(pk=pk)
 
-    @action(methods=["get"], detail=True)
-    def category(self, request, pk=None):
-        cats = Category.objects.get(pk=pk)
-        return Response({"cats": cats.title})
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+# class WomenViewSet(viewsets.ModelViewSet):
+#     # queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+#     def get_queryset(self):
+#         pk = self.kwargs.get("pk")
+
+#         if not pk:
+#             return Women.objects.all()
+
+#         return Women.objects.filter(pk=pk)
+#     @action(methods=["get"], detail=True)
+#     def category(self, request, pk=None):
+#         cats = Category.objects.get(pk=pk)
+#         return Response({"cats": cats.title})
