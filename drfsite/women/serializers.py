2c26f7be58e1aad8b21049b199b7317f7ae53dfa
Alexey Razmanov <razmanov666@gmail.com>
2022-08-23 18:15:25 +0300

    create "HiddenField" for current user

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -3,13 +3,9 @@ from rest_framework import serializers
 from .models import Women
 
 
-class WomenModel:
-    def __init__(self, title, content) -> None:
-        self.title = title
-        self.content = content
-
-
 class WomenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Women
         # fields = ('title', 'content', 'cat')
