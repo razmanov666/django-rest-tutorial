d5844ae0f8e287bd1f6905fa8148a6bfb0de254c
Alexey Razmanov <razmanov666@gmail.com>
2022-08-23 17:15:29 +0300

    customize 'queryset' & use dec-or '@action' for list category

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,25 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from .models import Category
 from .models import Women
 from .serializers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+
+        if not pk:
+            return Women.objects.all()
+
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=["get"], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({"cats": cats.title})
