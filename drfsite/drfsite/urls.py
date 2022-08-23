c757dfa2eed689b054e49e2f52f64a43384cacd6
Alexey Razmanov <razmanov666@gmail.com>
2022-08-23 18:14:24 +0300

    set urls

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -14,17 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include
 from django.urls import path
-from rest_framework import routers
-from women.views import WomenViewSet
+from women.views import WomenAPIDestroy
+from women.views import WomenAPIList
+from women.views import WomenAPIUpdate
+
+# from django.urls import include
+# from rest_framework import routers
+
 
 # router = routers.SimpleRouter()
-router = routers.DefaultRouter()
-router.register(r"women", WomenViewSet, basename="women")
+# router = routers.DefaultRouter()
+# router.register(r"women", WomenViewSet, basename="women")
 # print(router.urls)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include(router.urls)),
+    # path("api/v1/", include(router.urls)),
+    path("api/v1/women/", WomenAPIList.as_view()),
+    path("api/v1/women/<int:pk>/", WomenAPIUpdate.as_view()),
+    path("api/v1/womendelete/<int:pk>/", WomenAPIDestroy.as_view()),
 ]
