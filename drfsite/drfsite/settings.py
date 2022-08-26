68acce707a51c677eeac4f426346005e0b79c62e
Alexey Razmanov <razmanov666@gmail.com>
2022-08-26 13:27:33 +0300

    add djoser in "installed_apps" & add "default_auth_classes in rest_framework"

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "women.apps.WomenConfig",
     "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -133,7 +135,12 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
-    # 'DEFAULT_PERMISSIONS_CLASSES':[
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    "DEFAULT_PERMISSIONS_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
