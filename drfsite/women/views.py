328420be94a7165bbf01f701d1ce1a72410737ff
Alexey Razmanov <razmanov666@gmail.com>
2022-08-28 18:08:31 +0300

    set up the pagination

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -2,6 +2,7 @@
 # from rest_framework.decorators import action
 # from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
@@ -14,10 +15,17 @@ from .serializers import WomenSerializer
 # from .models import Category
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = "page_size"
+    max_page_size = 10000
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
